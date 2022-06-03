593d36617c6e4575f64cbcd7a0a41547ea791cd8
takuya hatanaka <trhchop0723@gmail.com>
2022-06-03 16:32:09 +0900

    Authentication function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from 'react';
+import styles from './App.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import Feed from './components/Feed';
+import Auth from './components/Auth';
 
-function App() {
+const App: React.FC = () => {
+  
+  const user = useSelector(selectUser);// useSelectorはReduxストアのステートにアクセスするのフック。
+  const dispatch = useDispatch();// Reduxのdispatch関数にアクセスするフック。
+
+  useEffect(() => {
+    // onAuthStateChangedは一度実行すると、ユーザーの変化の監視を始める。
+    // 戻り値は監視をやめるための関数なので、監視をやめる場合はその関数を実行する。この場合はunSub()
+    const unSub = auth.onAuthStateChanged((authUser) => {// authUser(変数名はなんでも良い)は変化後のユーザー情報が入ってくる。
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    // クリーンアップ関数はAppコンポーネントが破棄される時に実行される。
+    return () => {
+      unSub();
+    };
+  }, [dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      { user.uid ? (
+        <div className={styles.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
   );
 }
 
---
+++ src/components/Feed.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { auth } from "../firebase";
+
+const Feed = () => {
+  return (<div>
+    Feed
+    <button onClick={() => auth.signOut()}>LogOut</button>
+  </div>
+  );
+};
+
+export default Feed;
